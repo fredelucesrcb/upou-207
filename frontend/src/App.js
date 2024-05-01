de319385f6693c9fdfd43f53974441e06bb289ce
Roy Fredeluces <fredeluces.rcb@gmail.com>
2024-05-01 18:36:32 +0800

    frontend changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Outlet, Link } from "react-router-dom";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Layout = () => {
 
-export default App;
+const [email, setEmail] = useState("");
+const [message, setMessage] = useState("");
+
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    let res = await fetch("http://localhost:8000/apis/basic/", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: email,
+      }),
+    });
+    let resJson = await res.json();
+    if (res.status === 200) {
+      setEmail("");
+      setMessage("User created successfully");
+    } else {
+      setMessage("Some error occured");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+return (
+  <div className="App">
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={email}
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button type="submit">Subscribe</button>
+
+      <div className="message">{message ? <p>{message}</p> : null}</div>
+    </form>
+  </div>
+);
+};
+
+export default Layout;
